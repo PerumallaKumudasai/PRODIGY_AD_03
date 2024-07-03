b552b56731224302a16a8b43ecab740d26d31a0a
PerumallaKumudasai <perumallakumuda1105@gmail.com>
2024-07-03 19:59:13 +0530

    Add files via upload

---
+++ stopwatch.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(StopwatchApp());
+
+class StopwatchApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StopwatchHomePage(),
+    );
+  }
+}
+
+class StopwatchHomePage extends StatefulWidget {
+  @override
+  _StopwatchHomePageState createState() => _StopwatchHomePageState();
+}
+
+class _StopwatchHomePageState extends State<StopwatchHomePage> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
+  bool _isRunning = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _stopwatch = Stopwatch();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      if (_stopwatch.isRunning) {
+        setState(() {});
+      }
+    });
+  }
+
+  String _formatTime(int milliseconds) {
+    int hundreds = (milliseconds ~/ 10) % 100;
+    int seconds = (milliseconds ~/ 1000) % 60;
+    int minutes = (milliseconds ~/ (1000 * 60)) % 60;
+    return '${minutes.toString().padLeft(2, '0')}:'
+        '${seconds.toString().padLeft(2, '0')}:'
+        '${hundreds.toString().padLeft(2, '0')}';
+  }
+
+  void _startStopwatch() {
+    setState(() {
+      _isRunning = true;
+      _stopwatch.start();
+      _startTimer();
+    });
+  }
+
+  void _stopStopwatch() {
+    setState(() {
+      _isRunning = false;
+      _stopwatch.stop();
+      _timer.cancel();
+    });
+  }
+
+  void _resetStopwatch() {
+    setState(() {
+      _isRunning = false;
+      _stopwatch.reset();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stopwatch'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _formatTime(_stopwatch.elapsedMilliseconds),
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _isRunning ? _stopStopwatch : _startStopwatch,
+                  child: Text(_isRunning ? 'Pause' : 'Start'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _resetStopwatch,
+                  child: Text('Reset'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
